42f9c97bed10db4a4f1a9cac34b1e17e1451373d
Troy Anderson <Troyanderson.d@gmail.com>
2020-04-21 12:05:25 +0000

    updated script

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8080);
+server.listen(8080, () => console.log("running server")
+);
 
 io.on('connection', function (socket) {
+    console.log("connected")
     socket.on('join', function (data) {
         socket.join(data.roomId);
         socket.room = data.roomId;
